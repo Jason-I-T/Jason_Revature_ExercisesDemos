18ddad7c124c66031077f46dc009e0f5467ce50d
JasonTejadaRevature <jason263@revature.net>
2022-11-17 10:13:50 -0800

    Added battle function for pokemon

--- pokemonApp/Program.cs
+++ pokemonApp/Program.cs
@@ -1,4 +1,14 @@
-﻿namespace pokemonApp;
+﻿/*
+Battle imp:
+    battle loop - two pokemon
+        array of moves, each entry int value for damage... multiplier maybe based on effectiveness
+        user has choice of 1-4 , does damage to computer
+        randomly choose move (1-4) for computer, does damange to user
+        whoever hits 0 (or less) loses
+        loop ends
+*/
+
+namespace pokemonApp;
 class Program
 {
     static void Main(string[] args)
@@ -6,6 +16,13 @@ class Program
         //Object instantiation
         Pokemon charmander = new Pokemon("charmander");
         Pokemon squirtle = new Pokemon("squirtle");
+
+        int[] pikaMoves = {0, 3, 4, 5};
+        Pokemon pikachu = new Pokemon("Pikachu", 10, pikaMoves);
+        int[] bulbaMoves = {1, 2, 3, 4};
+        Pokemon bulbasaur = new Pokemon("Bulbasaur", 10, bulbaMoves);
+
+        Console.WriteLine(Pokemon.Battle(pikachu, bulbasaur));
         
         //You can use dot notation to access fields and methods available to an object of that class.
         charmander.type = "fire";
@@ -27,7 +44,6 @@ class Program
 class Pokemon 
 {
     //Fields
-
     //Fields are private by default. This field was explicitly declared private.
     private string name;
     
@@ -44,6 +60,8 @@ class Pokemon
     
     int weight;
     int level;
+
+    int[] moves = new int[4];
 
     //Constructors - Special methods used to instantiate or "create" an instance of a class.
     //You can have as many constructors as you need provided the signatures are different. 
@@ -71,8 +89,13 @@ class Pokemon
         level = lvl;
     }
 
-    //Methods
+    public Pokemon(string name, int hp, int[] moves) {
+        this.name = name;
+        hitpoints = hp;
+        this.moves = moves; // defined when pokemon is instantiated
+    }
 
+    //Methods
     //This method is an instance method. It can be called by an object of class Pokemon using dot-notation.
     public void IsPokemon()
     {
@@ -83,6 +106,41 @@ class Pokemon
     public static void Sound()
     {
         Console.WriteLine("*pokemon noises*");
+    }
+
+    public static string Battle(Pokemon a, Pokemon b) {
+        Random rand = new Random();
+        int aHP = a.hitpoints, bHP = b.hitpoints;
+        
+        Console.WriteLine($"Started battle between {a.name} and {b.name}!");
+        
+
+        int moveIndex = 0;
+        do {
+            Console.WriteLine($"Your HP: {aHP}\nEnemey HP: {bHP}!");
+            Console.WriteLine("Choose Move, Enter 1-4...");
+            string? choice = Console.ReadLine();
+            if(choice.Equals("1") || choice.Equals("2") || choice.Equals("3") || choice.Equals("4") ) {
+                    bool pleaseWork = Int32.TryParse(choice, out moveIndex);
+            } else {
+                Console.WriteLine("Do it right thsi time (1-4)");
+                continue;
+            }
+
+            // the user chose their move, now do damage
+            bHP -= a.moves[--moveIndex];
+            Console.WriteLine($"You did {a.moves[moveIndex]} damange to {b.name}");
+            if(bHP <= 0)
+                return "You win!!";
+            
+            int compChoice = rand.Next(0,4);
+            aHP -= b.moves[compChoice];
+            Console.WriteLine($"Enemey did {b.moves[compChoice]} damange to {a.name}");
+            if(aHP <= 0)
+                return "You lose!!"; 
+
+        } while(aHP > 0);
+        return "";
     }
 
     //Overriding
