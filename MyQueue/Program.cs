90c5e44b3d8ae5386723e18bcddfdf0825f61020
JasonTejadaRevature <jason263@revature.net>
2022-11-22 09:32:21 -0800

    Added my implementation of a queue

---
+++ MyQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MyQueue;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        MyQueue<string> myQ = new MyQueue<string>();
+        MyQueue<string> myOtherQ = new MyQueue<string>();
+
+        //myOtherQ.enqueue(null);
+
+        myQ.enqueue("please");
+        myQ.enqueue("work");
+        myQ.enqueue("im");
+        myQ.enqueue("begging");
+
+        Console.WriteLine(myQ.peek());
+        Console.WriteLine(myQ.peek());
+        Console.WriteLine(myQ.peek());
+
+        myQ.printQueue();
+
+        Console.WriteLine(myQ.Contains("work"));
+        Console.WriteLine(myQ.Contains("dont"));
+        //Console.WriteLine(myOtherQ.Contains(null));
+
+        myQ.Clear();
+        Console.WriteLine(myQ.Size());
+    }
+}
