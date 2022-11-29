66e5fca365d5a5c3cd3c9ea036dae98f9c1b6a89
JasonTejadaRevature <jason263@revature.net>
2022-11-29 09:36:28 -0800

    Expanded on the demo...

--- ConsumeApiDemo/Item.cs
+++ ConsumeApiDemo/Item.cs
@@ -14,10 +14,22 @@ public class Item {
     "carryLimit": int,
     "value": int
     */
-    public int id {get; set;}
+        
+            public int id {get; set;}
     public string ?name {get; set;}
     public string ?description {get; set;}
     public int rarity {get; set;}
     public int carryLimit {get; set;}
     public int value {get; set;}
+
+    public Item() {}
+    public Item(int id, string name, string description, int rarity, int carryLimit, int value) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.rarity = rarity;
+        this.carryLimit = carryLimit;
+        this.value = value;
+   
+    }
 }
