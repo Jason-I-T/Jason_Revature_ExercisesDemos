d24f1c2deb983f5d5061829351b70f41e59addf4
JasonTejadaRevature <jason263@revature.net>
2022-12-14 07:35:28 -0800

    Started web demo for javascript (in the wrong branch)

---
+++ WebDemos/exampleJS.js
@@ -0,0 +1,10 @@
+"use strict"; // Included to prevent any legacy code weirdness
+
+// IN JS, "//" is comments
+// In C#, to make int -> specifically request an int
+// In JS, we use dynamic typing to determine the data type of vars
+
+var number = 1;
+
+
+console.log(number)
