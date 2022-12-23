873bb496b2e1a05cf7ee91f559c18fb49c6ee3f0
JasonTejadaRevature <jason263@revature.net>
2022-12-23 14:21:11 -0800

    Started on liberating hero detail from hero component, and adding/adding routes to... its own view

--- TourOfHeroes/my-tutorial/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/my-tutorial/src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service'
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,22 +9,15 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   // Injecting hero service
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
   
   getHeroes(): void {
-    //this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
