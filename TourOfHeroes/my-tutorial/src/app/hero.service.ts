1492d5d49f34039a7179542bcb0ae24de110efc9
JasonTejadaRevature <jason263@revature.net>
2022-12-23 06:43:31 -0800

    Added messages component and service (this was basically a logger)

--- TourOfHeroes/my-tutorial/src/app/hero.service.ts
+++ TourOfHeroes/my-tutorial/src/app/hero.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // Getter
   getHeroes(): Observable<Hero[]> { // Get list of heroes asynchronously
     const heroes = of(HEROES); // 'of' returns Observable<T>
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
