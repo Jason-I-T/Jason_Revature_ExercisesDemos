ed41e9fb7d3e19154eb11b2de0995ec21a632081
JasonTejadaRevature <jason263@revature.net>
2022-12-23 14:35:27 -0800

    Split up hero detail to give its own view, added a back button to navigate the application...

--- TourOfHeroes/my-tutorial/src/app/hero.service.ts
+++ TourOfHeroes/my-tutorial/src/app/hero.service.ts
@@ -11,10 +11,16 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  // Getter
+  // Getters
   getHeroes(): Observable<Hero[]> { // Get list of heroes asynchronously
     const heroes = of(HEROES); // 'of' returns Observable<T>
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
