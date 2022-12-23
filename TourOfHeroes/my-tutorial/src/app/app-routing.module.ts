5bbd9c6c172c15258d9c04afa1f4117fddfef927
JasonTejadaRevature <jason263@revature.net>
2022-12-23 13:50:04 -0800

    Added routing!

--- TourOfHeroes/my-tutorial/src/app/app-routing.module.ts
+++ TourOfHeroes/my-tutorial/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes=[
+  { path: 'heroes', component: HeroesComponent}
+];
 
-@NgModule({
+// Initializes the router, listens for browser location changes
+@NgModule({ 
+  /* 
+  Adds RouterModule to AppRoutingModule imports array & configures with routes in one step.
+  Called forRoot b/c configure router at app's root level. Gives data needed for routing, performs 
+  initial navibation based on current browser URL 
+  */
   imports: [RouterModule.forRoot(routes)],
+  // Makes RouterModule to be available throught the application
   exports: [RouterModule]
 })
+
+// Add this module to the imports array in app.module.ts to make it work
 export class AppRoutingModule { }
