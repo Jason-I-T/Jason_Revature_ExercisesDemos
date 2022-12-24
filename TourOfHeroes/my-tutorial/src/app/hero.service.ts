dacf90c1b2fad8ead2c4fd1fdddac5d373de3b68
JasonTejadaRevature <jason263@revature.net>
2022-12-24 05:15:43 -0800

    Added a mock server with In Memory Web Api. Get request to get our heroes instead of storing a list. Simulating calls to a backend.

--- TourOfHeroes/my-tutorial/src/app/hero.service.ts
+++ TourOfHeroes/my-tutorial/src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
@@ -8,19 +10,25 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   // Getters
-  getHeroes(): Observable<Hero[]> { // Get list of heroes asynchronously
-    const heroes = of(HEROES); // 'of' returns Observable<T>
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-  }
+ /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesUrl)
+}
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
